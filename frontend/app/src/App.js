5396914b243ab54b22730927182eb06b2dbd5137
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-08-15 16:03:13 -0300

    feat: printando no console caso um usuário já exista

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
 import './App.css';
 import Cadastro from './components/Cadastro/Cadastro';
-import Listar from './components/Listar/Listar';
 
 function App() {
   return (
